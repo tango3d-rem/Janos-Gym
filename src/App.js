bc2f84d0b99d6372799a7301c88f5ee5858f7c37
mario <marius.achu@gmail.com>
2025-03-29 15:16:31 -0300

    organizando proyecto en react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import './App.css';
+// App.js
+import "./css/assets/styles.css"; // Importa tu CSS global
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Tu contenido JSX aquí */}
     </div>
   );
-}
+};
 
 export default App;
