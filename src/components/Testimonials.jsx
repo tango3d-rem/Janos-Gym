aba35981e4f957a28cf8edd59f0556b792d5d095
mario <marius.achu@gmail.com>
2025-03-30 15:42:42 -0300

    va quedando mejor1

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,28 +6,33 @@ const Testimonials = () => {
     {
       text: "Desde que entreno en Janos Gym, mi energía ha mejorado y he logrado mis objetivos de fitness.",
       author: "Carlos G.",
-      rating: 5
+      rating: 5,
     },
 
     {
       text: "Los entrenadores están altamete calificados y el ambiente es increíble. ¡Muy recomendado!",
       author: "Ana R.",
-      rating: 5
+      rating: 5,
     },
 
     {
       text: "Janos Gym es el mejor gimnacio al que he asistido, con rutinas efectivas y motivación constante",
       author: "Luis M.",
-      rating: 5
+      rating: 5,
     },
     // Agrega más testimonios aquí
   ];
 
   // Función para generar estrellas
   const renderStars = (rating) => {
-    return "★".repeat(rating).split("").map((star, i) => (
-      <span key={i} className="star">{star}</span>
-    ));
+    return "★"
+      .repeat(rating)
+      .split("")
+      .map((star, i) => (
+        <span key={i} className="star">
+          {star}
+        </span>
+      ));
   };
 
   return (
