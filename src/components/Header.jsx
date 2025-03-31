7027b2ab7ba62ec5144873f5854c63bd7102e074
mario <marius.achu@gmail.com>
2025-03-30 22:46:29 -0300

    dando formato

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,26 @@ const Header = () => {
     <header className="header">
       <nav className="navbar">
         <div className="logo">
-        <a href="/">JANO´S GYM</a>
+          <a href="/">JANO´S GYM</a>
         </div>
         <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-          <li><a href="#productos">Productos</a></li>
-          <li><a href="#coaching">Coaching</a></li>
-          <li><Link to="#registrarse" className="cta-button">Registrarse</Link></li>
-          <li><Link to="#ingresar">Ingresar</Link></li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#productos">Productos</a>
+          </li>
+          <li>
+            <a href="#coaching">Coaching</a>
+          </li>
+          <li>
+            <Link to="#registrarse" className="cta-button">
+              Registrarse
+            </Link>
+          </li>
+          <li>
+            <Link to="#ingresar">Ingresar</Link>
+          </li>
         </ul>
       </nav>
     </header>
